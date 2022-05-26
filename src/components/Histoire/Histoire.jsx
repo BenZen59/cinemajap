100af809e504e57836799d72a63b181f3137e758
BenZen59 <benoit.zenone@gmail.com>
2022-05-27 01:06:05 +0200

    update

--- src/components/Histoire/Histoire.jsx
+++ src/components/Histoire/Histoire.jsx
@@ -1,9 +1,188 @@
 import './Histoire.css';
+import VoyageTokyo from '../../image/Voyage_tokyo.jpg';
+import ConteLune from '../../image/contes_de_la_lune.jpg';
+import Orochi from '../../image/orochi.jpg';
+import Godzilla from '../../image/godzilla.jpg';
 
 export default function Histoire() {
   return (
     <div className='histoire'>
-      <p>Histoire du cinéma japonais</p>
+      <div>
+        <h1>Histoire du cinéma japonais</h1>
+        <h2>Les débuts</h2>
+        <p className='texte'>
+          Les premiers films, ceux de Thomas Edison qui adopte le mot anglais
+          film pour désigner les bobineaux enregistrés avec la première caméra
+          de cinéma, le Kinétographe, par son assistant William Kennedy Laurie
+          Dickson, sont connus des Japonais dès novembre 1896 car ils sont
+          présentés à Kobe à l'aide des kinétoscopes, les appareils de
+          visionnement individuel mis au point par Dickson d'après les croquis
+          de l'industriel américain.
+          <br />
+          <br />
+          Puis ce sont deux opérateurs des frères Lumière, Gabriel Veyre et
+          François-Constant Girel, qui organisent des projections sur grand
+          écran à Osaka en 1897 à l'aide d'un ses concurrents français est faite
+          à Osaka puis à Tokyo, mais la première caméra importée au Japon par
+          Shirō Asano porte la marque Lumière.
+          <br />
+          <br /> C'est Shibata Tsunekichi qui commence à tourner les premiers
+          films : il s'agit de scènes de rues et de geishas.
+        </p>
+        <h2>Le cinéma muet et les débuts du cinéma parlant</h2>
+        <p className='texte'>
+          La première star japonaise est un acteur de kabuki, Matsunosuke Onoe,
+          qui apparaît dans près d'un millier de films entre 1909 et 1926.
+          <br />
+          <br /> La première actrice reconnue est la danseuse classique Tokuko
+          Nagai Takagi, qui apparaît dans quatre films produits par la compagnie
+          américaine Thanhouser entre 1911 et 1914.
+          <br />
+          <br /> Orochi (1925) de Buntarō Futagawa. Shōzō Makino popularise le
+          genre jidaigeki.
+          <br />
+          <br /> Il tourne en décors naturels, prologue à la sortie du film
+          japonais de l'univers théâtral. Les films sont encore muets, et les
+          cinémas emploient des benshi, qui commentent ou interprètent la
+          bande-son des films, parfois accompagnés de musique jouée par un
+          orchestre.
+          <br />
+          <br /> Leur grande popularité explique en partie le retard du Japon à
+          passer massivement au cinéma parlant dans la seconde moitié des années
+          1930. <br />
+          <br />
+          Il ne subsiste que très peu de films de cette époque, car ils ont été
+          détruits par le tremblement de terre de 1923 ou les bombardements de
+          la Seconde Guerre mondiale. Le séisme de 1923 inspire des mélodrames
+          larmoyants comme La chansonnette du passeur de Daisuke Itō.
+          <br />
+          <br /> Gosses de Tokyo (1932) de Yasujirō Ozu. Daisuke Itō est
+          peut-être le premier cinéaste, au sens d'utiliser le médium cinéma
+          comme expression artistique en tant que telle et non seulement comme
+          une production industrielle, lorsqu'il réalise en 1927 Journal de
+          voyage de Chûji, après avoir écrit de nombreux scénarios de commande.
+          <br />
+          <br /> Au même moment Teinosuke Kinugasa, acteur de kabuki, réalise
+          des films marqués par une forte influence européenne.
+          <br />
+          <br /> C'est le temps des films « à tendance » qui, sur fond de crise
+          économique et sociale, tentent d'illustrer les conséquences négatives
+          et contradictions du capitalisme avant que la censure ne mette fin à
+          cette ambition critique.
+          <br />
+          <br /> C'est ce contexte qui favorise l'émergence ou impose une autre
+          orientation : libéré des contraintes figées du théâtre, l'intérêt pour
+          la vie quotidienne au sein du foyer japonais se développe afin
+          d'éviter les thèmes idéologiques.
+          <br />
+          <br /> C'est le début du premier âge d'or du cinéma japonais, avec
+          Gosses de Tokyo de Yasujirō Ozu (1932), L'Élégie d'Osaka et Les Sœurs
+          de Gion de Kenji Mizoguchi (1936).
+          <br />
+          <br /> En 1936, les studios de la compagnie Shōchiku quittent le
+          quartier de Kamata à Tokyo pour s'installer à Ōfuna (ja), dans la
+          préfecture de Kanagawa, et commencent à promouvoir des stars comme
+          Kinuyo Tanaka ou Hiroko Kawasaki. Le 26 février 1936, des officiers
+          fomentent un coup d'État qui échoue mais qui symbolise l'essor
+          inexorable du militarisme.
+          <br />
+          <br /> La veille, l'Association des réalisateurs japonais était
+          fondée, ce qui permet à Tomu Uchida de réaliser Le Progrès éternel
+          (1937) sur une idée d'Ozu, au moment même où débute la guerre
+          sino-japonaise.
+          <br />
+          <br />
+          En 1937, Sadao Yamanaka réalise son dernier film, Pauvres humains et
+          ballons de papier, tenu par Kiyoshi Kurosawa comme le chef-d'œuvre du
+          cinéma japonais, œuvre dominée par le thème de la mort. Yamanaka meurt
+          l'année suivante sur le front chinois, à 28 ans.
+        </p>
+        <h2>Pendant la guerre</h2>
+        <p className='texte'>
+          Au début de la guerre sino-japonaise, une loi mettant la production
+          cinématographique sous contrôle du gouvernement est mise en place le
+          1er octobre 1939.
+          <br />
+          <br /> Les professionnels doivent avoir une autorisation du pouvoir
+          japonais. Ainsi, la censure est appliquée avant même les tournages.
+          <br />
+          <br /> Cette même année, Le Goût du riz au thé vert de Yasujirō Ozu ne
+          passe pas cette censure préalable ; décrire l'oisiveté de femmes
+          bourgeoises n'est pas autorisé en temps de guerre.
+          <br />
+          <br /> En 1940, les autorités vont jusqu'à interrompre une projection
+          du documentaire Les Soldats au combat et retirer son droit d'exercer
+          au réalisateur Fumio Kamei pour ses idées marxistes.
+          <br />
+          <br /> Le chef-d'œuvre de Tomotaka Tasaka, Terres et soldats (1939),
+          décrit les souffrances de la guerre tout en exaltant le militarisme
+          nippon.
+          <br />
+          <br /> Des films ne parlant pas directement de la guerre sont acceptés
+          comme la trilogie de l'art réalisée par Kenji Mizoguchi d'après des
+          scénarios de Yoshikata Yoda : Conte des chrysanthèmes tardifs (1939),
+          La Femme de Naniwa (1940) et La Vie d'un acteur (1941) sont une
+          apologie du sacrifice de soi, finalement proches des thèmes des films
+          militaristes.
+          <br />
+          <br /> En 1941, alors que la guerre prend de l'ampleur, le « Bureau
+          d'information publique » veut limiter la production en ne gardant que
+          deux geki eiga (films de fiction) par mois produits par deux
+          compagnies seulement, alors que la Nikkatsu, Shōchiku, Tōhō, Shinko et
+          Daito présentent alors environ un nouveau film par semaine.
+          <br />
+          <br /> Les différentes compagnies sont fusionnées en deux compagnies :
+          la Shōchiku et la Tōhō. Néanmoins, Masaichi Nagata de Shinkō Kinema
+          intervient pour permettre la création en 1942 de ce qui deviendra la
+          Daiei.
+          <br />
+          <br /> Beaucoup de professionnels abandonnent leur emploi. Les jeunes
+          employés partent à la guerre. Dans les territoires occupés comme les
+          Philippines, l'Indonésie ou la Mandchourie, des films de propagande
+          sont tournés11. Kajirō Yamamoto réalise des parodies avec le comique
+          Enoken avant de réaliser des films de propagande militaristes dans
+          lesquels sont expérimentées des techniques de prises de vue qui seront
+          reprises sur le plateau des Godzilla.
+          <br />
+          <br /> Tous les genres contribuent à la propagande. La Vengeance des
+          47 rōnin (en deux parties, 1941-1942), un reshiki-geki (dramatique
+          historique) fleuve de 3 h 35 de Mizoguchi, reconstitue très
+          esthétiquement l'histoire célèbre des 47 rōnin. Ozu tourne en 1942 Il
+          était un père qui décrit un père ayant un sens élevé de ses
+          responsabilités. Le scénario est donc conforme aux idées de l'État
+          dans la guerre bien que le scénario ait été écrit en 1937.
+          <br />
+          <br />
+          Keisuke Kinoshita cède lui aux exigences de la propagande avec, en
+          1943, Le Port en fleurs, tout en réalisant une comédie populaire. Mais
+          son manque d'enthousiasme militariste l'écartera de la réalisation de
+          Kamikazes, un film patriotique.
+          <br />
+          <br /> En 1943, l'exemption de service militaire pour les étudiants
+          est levée. En 1945, neuf membres de la compagnie de théâtre Sakuratai
+          meurent dans le bombardement d'Hiroshima.
+        </p>
+      </div>
+      <div className='pictureRight'>
+        <img src={VoyageTokyo} alt='voyageTokyo' />
+        <p className='description'>
+          Affiche du film Voyage à Tokyo de <br />
+          Yasujirō Ozu.
+        </p>
+        <img src={ConteLune} alt='contelune' />
+        <p className='description'>
+          Affiche du film Les Contes de la
+          <br /> lune vague après la pluie de Kenji
+          <br /> Mizoguchi.
+        </p>
+        <img src={Orochi} alt='orochi' />
+        <p className='description'>
+          Orochi (1925) de Buntarō
+          <br /> Futagawa.
+        </p>
+        <img className='godzilla' src={Godzilla} alt='godzilla' />
+        <p className='description'>Affiche du film Godzilla (1954).</p>
+      </div>
     </div>
   );
 }
