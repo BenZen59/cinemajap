778b6973a859bf640fb683fc232f9e28b715edea
BenZen59 <benoit.zenone@gmail.com>
2022-05-27 14:36:31 +0200

    update

--- src/components/Histoire/Histoire.jsx
+++ src/components/Histoire/Histoire.jsx
@@ -3,6 +3,7 @@ import VoyageTokyo from '../../image/Voyage_tokyo.jpg';
 import ConteLune from '../../image/contes_de_la_lune.jpg';
 import Orochi from '../../image/orochi.jpg';
 import Godzilla from '../../image/godzilla.jpg';
+import Kitano from '../../image/kitano.jpg';
 
 export default function Histoire() {
   return (
@@ -162,6 +163,274 @@ export default function Histoire() {
           est levée. En 1945, neuf membres de la compagnie de théâtre Sakuratai
           meurent dans le bombardement d'Hiroshima.
         </p>
+        <h2>L'après-guerre</h2>
+        <p className='texte'>
+          À la censure japonaise succède la censure imposée par les Américains.
+          Akira Kurosawa fait ses débuts comme assistant de Kajirō Yamamoto
+          durant la guerre.
+          <br />
+          <br /> En 1946, sort Je ne regrette pas ma jeunesse, virulente
+          critique du système qui vient de s'écrouler. La même année, Keisuke
+          Kinoshita, qui a aussi débuté durant la guerre, réalise Le Matin de la
+          famille Osone.
+          <br />
+          <br /> En 1951, Rashōmon, avec la star Toshirō Mifune, reçoit le Lion
+          d'or à Venise puis l'Oscar du meilleur film étranger.
+          <br />
+          <br /> Cette récompense stimule l'ambition en berne des aînés. Les
+          Contes de la lune vague après la pluie de Kenji Mizoguchi (1953) et
+          Les Sept Samouraïs (1954) de Kurosawa sont récompensés par un Lion
+          d'argent à la Mostra de Venise. Masaki Kobayashi reçoit le Prix du
+          jury du Festival de Cannes pour Hara-kiri en 1962.
+          <br />
+          <br /> C'est le deuxième âge d'or. Les studios tournent également de
+          très nombreux films de genre.
+          <br />
+          <br /> C'est le début des kaijū-eiga (films de monstres) avec Godzilla
+          d'Ishirō Honda en 1954.
+          <br />
+          <br /> Durant l'après-guerre, la Nikkatsu qui s'était limitée à la
+          distribution après 1941 distribue des films américains puis décide de
+          produire de nouveau des films. De nouveaux studios Nikkatsu sont
+          construits en 1954 dans la banlieue de Tokyo.
+          <br />
+          <br /> La Nikkatsu lance la star Yūjirō Ishihara avec l'adaptation de
+          deux romans de Shintarō Ishihara, un écrivain de la « génération du
+          soleil » (taiyōzoku) : La Saison du soleil (Takumi Furukawa, 1956) et
+          Passions juvéniles (Kō Nakahira, 1956).
+          <br />
+          <br /> Le succès de ces films entraine l'adoption d'une ligne de
+          production de films estampillés Nikkatsu Action, fictions dont le
+          fonds de commerce repose sur la violence et la sexualité débridées de
+          héros de type « jeunes rebelles », notamment dans les pinku eiga
+          (films érotiques).
+        </p>
+        <h2>La nouvelle vague</h2>
+        <p className='texte'>
+          La nouvelle vague japonaise, contrairement à la Nouvelle Vague
+          française, ne regroupait pas un groupe de cinéastes autour d'une revue
+          ou d'un groupe, mais correspondait au Japon à un terme utilisé par les
+          critiques pour évoquer des cinéastes « rebelles » de la Shōchiku :
+          Nagisa Ōshima, Yoshishige Yoshida et Masahiro Shinoda et en référence
+          à la Nouvelle Vague française.
+          <br />
+          <br /> Les trois réalisateurs s'opposaient aux « maîtres » des studios
+          tels que Keisuke Kinoshita et Yasujirō Ozu, accusés de réaliser un
+          cinéma « bourgeois ».
+          <br />
+          <br /> Dès son deuxième film, Contes cruels de la jeunesse (1960), qui
+          aborde le renouvellement du traité de sécurité américano-japonais,
+          Ōshima filme une histoire mêlant sexe et crime, des thèmes qui
+          parcourront son œuvre. Le film est retiré de l'affiche après quatre
+          jours et Ōshima quitte les studios pour fonder sa société
+          indépendante.
+          <br />
+          <br /> Dans le même temps d'autres réalisateurs qui ne sont pas passés
+          par les studios se font connaître, comme Susumu Hani et Hiroshi
+          Teshigahara, qui débutent en réalisant des documentaires.
+          <br />
+          <br /> C'est aussi l'essor des productions indépendantes, produites
+          grâce à un système de collaboration entre une petite société de
+          distribution, l'Art Theatre Guild, et une société de production
+          dirigée par le réalisateur. D'autres films sortent selon ce système
+          comme La Pendaison de Nagisa Ōshima en 1968.
+          <br />
+          <br /> Ce modèle de financement basé sur de petits budgets permet à de
+          nombreux réalisateurs et à des idées nouvelles d'émerger, comme L'Île
+          nue de Kaneto Shindō.
+          <br />
+          <br /> À la Nikkatsu, Shōhei Imamura tourne Désir inassouvi
+          (Hateshinaki Yokubo) en 1958 ou La Femme insecte (1963), portrait
+          d'une prostituée luttant pour son indépendance, qui sont
+          caractéristiques de son regard d'« entomologiste » de la société
+          japonaise.
+          <br />
+          <br /> Alors que la fréquentation totale des salles baisse à partir de
+          1959, apparaissent des petites sociétés spécialisées dans la
+          production de films érotiques ou pinku-eiga qui attirent un large
+          public.
+          <br />
+          <br /> Tetsuji Takechi, critique influent et metteur en scène de
+          théâtre traditionnel, décide de réaliser des pinku : Neige noire est
+          saisi par la police et Tetsuji poursuivi pour violation des lois sur
+          l'obscénité.
+          <br />
+          <br /> En 1968, Seijun Suzuki qui a passé sa carrière à tenter de
+          produire des œuvres stylisées dans le cadre du studio Nikkatsu est mis
+          à la porte à la suite de son film La Marque du tueur.
+          <br />
+          <br /> La même année, deux documentaires marquent les esprits : Un été
+          à Narita de Shinsuke Ogawa montre les manifestations de paysans et
+          d'étudiants contre la construction du nouvel aéroport de Tokyo en
+          pleine campagne ; La préhistoire des partisans de Noriaki Tsuchimoto
+          suit le meneur de la rébellion étudiante à l'Université de Kyoto.
+          <br />
+          <br /> C'est à cette époque que Kōji Wakamatsu et Masao Adachi
+          réalisent l'essentiel de leurs films, croisant les codes du pinku
+          eiga, du film de yakuza et la critique sociale virulente.
+        </p>
+        <h2>Les années 1970</h2>
+        <p className='texte'>
+          En 1971, le premier film de Shūji Terayama Jetez vos livres et
+          descendez dans la rue ! est produit selon le système de collaboration
+          entre une société de distribution et le réalisateur.
+          <br />
+          <br /> La même année le critique Eizu Ori écrit à propos de La
+          Cérémonie de Nagisa Ōshima qu'il s'agit d'une synthèse prématurée de
+          la démocratie d'après-guerre.
+          <br />
+          <br /> L'époque est au pessimisme : Yukio Mishima s'est suicidé en
+          1970, en 1971 Masao Adachi part pour le Liban, en 1972 l'Armée rouge
+          japonaise tourne ses armes contre 12 de ses propres membres, les
+          survivants sont ensuite arrêtés à l'issue d'un siège qui bat tous les
+          records d'audience à la télévision.
+          <br />
+          <br /> Pour Nagisa Ōshima, c'est la fin du rôle des jeunes dans
+          l'histoire moderne du Japon. Les films de yakuza ont le vent en poupe
+          : Kinji Fukasaku filme de jeunes délinquants qui enfreignent toutes
+          les règles et par là se condamnent à une mort violente et prématurée
+          comme dans Combat sans code d'honneur.
+          <br />
+          <br /> C'est aussi l'époque des premiers films de la série Otoko wa
+          tsurai yo (C'est dur d'être un homme) de Yōji Yamada, saga populaire
+          aux thèmes universels.
+          <br />
+          <br /> En 1972, la police saisit quatre films roman porno (pinku-eiga
+          de la Nikkatsu) et neuf personnes sont inculpées. Le genre attire
+          néanmoins des créateurs au sommet de leur art.
+          <br />
+          <br /> C'est dans ce contexte qu'Oshima réalise en 1976, grâce à un
+          producteur français, L'empire des sens qui repousse les limites de
+          l'expression de la sexualité au Japon. Les livres qui présentent le
+          scénario et des photos du film sont saisis par les autorités
+          japonaises, le film est censuré et n'est jamais sorti au Japon en
+          version intégrale.
+        </p>
+        <h2>Les années 1980</h2>
+        <p className='texte'>
+          Les années 1980 et 1990 signent la mort du système des grands studios.
+          L'industrie du cinéma se reforme autour de producteurs et de
+          réalisateurs indépendants.
+          <br />
+          <br /> Les cinéastes de l'après-guerre continuent de tourner avec des
+          productions souvent non japonaises (Kurosawa en URSS, États-Unis,
+          France ; Ōshima en France).
+          <br />
+          <br /> La Ballade de Narayama de Shōhei Imamura gagne la Palme d'or en
+          1983. Les jidaigeki d'Akira Kurosawa Kagemusha, l'Ombre du guerrier
+          (1980, produit par Hollywood) et Ran (1985, production
+          franco-japonaise) remportent aussi de nombreux prix.
+          <br />
+          <br /> Shōhei Imamura gagne une nouvelle Palme d'or avec L'Anguille en
+          1997. Tous les réalisateurs apparus après 1980 sont nés après la
+          guerre et n'ont jamais travaillé pour les studios.
+          <br />
+          <br /> Takeshi Kitano qui a commencé par des manzai (sketchs de
+          cabaret) sous le nom de Beat Takeshi est engagé par Nagisa Ōshima pour
+          son film Furyo (1983).
+          <br />
+          <br /> En 1989, il remplace Kinji Fukasaku pour la réalisation de
+          Violent Cop. Il remanie le scénario en créant son personnage de héros
+          ambigu, dépeignant la société moderne comme règne de la violence
+          instinctive.
+          <br />
+          <br /> Shinji Sōmai dépeint dans Typhoon Club (1985) les affres de la
+          condition des jeunes Japonais désormais voués à la compétition sociale
+          dès leur plus jeune âge et considérés par les producteurs japonais
+          comme une masse se contentant de divertissements violents et/ou
+          érotiques. Il n'y a plus d'alternative à la société capitaliste
+          industrielle moderne. Les taux de suicide explosent.
+        </p>
+        <h2>Les années 1990</h2>
+        <p className='texte'>
+          Manque de communication, effritement des rapports humains et
+          dissolution des identités sociales sont des thèmes récurrents de cette
+          période.
+          <br />
+          <br /> Le Scintillement de Jōji Matsuoka (1992) évoque une famille qui
+          essaie d'inventer de nouveaux modes de coexistence différents du foyer
+          traditionnel.
+          <br />
+          <br /> Le personnage de Takeshi Kitano dans Sonatine (1993) illustre
+          ce nouveau rapport au monde, problématique et sans repères ; même le
+          gangster violent n'a plus sa place dans la société lorsqu'il est trop
+          vieux. Là encore, la seule issue pour le personnage de Kitano est le
+          suicide.
+          <br />
+          <br /> C'est aussi l'époque de l'émergence de réalisateurs étrangers
+          vivants au Japon, comme De quel côté se trouve la lune de Yōichi Sai
+          (1993) qui est un zainichi, c'est-à-dire un Coréen du Japon.
+          <br />
+          <br /> Alors que les scénarios de films d'horreur étaient jusqu'à
+          présent refusés par les producteurs, à la fin des années 1990 des
+          films d'horreur remportent un succès commercial comme Ring de Hideo
+          Nakata (1997) et/ou un succès critique comme Cure (1997) de Kiyoshi
+          Kurosawa, jusqu'à parfois faire l'objet de l'objet de remake des
+          studios américains.
+          <br />
+          <br /> Si les films d'horreur occidentaux sont des références pour ces
+          réalisateurs, ils développent néanmoins un traitement formel qui
+          s'impose comme « histoires de fantômes japonais » ou J-Horror.
+          <br />
+          <br /> Ces films caractérisés par une « horreur glacée » ne sont pas
+          sans être irrigués par la description de l'effacement des liens
+          sociaux remplacés par des prothèses électroniques.
+          <br />
+          <br /> Dans cette optique, Shin'ya Tsukamoto peut être considéré comme
+          le précurseur thématique, mais pas formel, de ce genre avec Tetsuo
+          (1989), bien que Kurosawa indique que plusieurs de ses scénarios
+          d'horreurs ont été refusés depuis les années 1980.
+          <br />
+          <br /> Le cinéma japonais aborde à cette époque des thèmes qui sont en
+          passe de devenir internationaux dans les années 2000, avec le
+          développement et la démocratisation de ce qu'on appelle alors les NTIC
+          pour Nouvelles Technologies de l'Information et de la Communication
+          (images numériques, ordinateurs personnels, téléphones portables et
+          Internet).
+        </p>
+        <h2>Les années 2000</h2>
+        <p className='texte'>
+          Hirokazu Kore-eda, Shinji Aoyama, Nobuhiro Suwa produisent des œuvres
+          influencées par le professeur spécialiste de littérature et de
+          philosophie française Shigehiko Hasumi, et continuent à dépeindre la
+          famille japonaise comme lieu d'expression privilégié des
+          bouleversements de la société dans son ensemble.
+          <br />
+          <br /> Kiyoshi Kurosawa, élève d'Hasumi comme Aoyama mais plus vieux
+          peut également être inscrit dans ce mouvement malgré les quelques
+          éléments fantastiques qui servent plutôt de prétextes.
+          <br />
+          <br /> Éléments fantastiques abandonnés à la fin des années 2000 dans
+          Tokyo Sonata (2008). Ce mouvement est appelé Rikkyo nūberu bāgu ou
+          Nouvelle vague Rikkyo du nom de l'université dans laquelle enseigne
+          Hasumi, et pour la distinguer de la Shochiku nuberu bagu des années
+          1960.
+          <br />
+          <br /> Sono Sion se fait connaître avec Suicide Club en 2001, très
+          proche à la fois formellement et thématiquement de ce que fait
+          Kurosawa à la même époque. À partir de 2005 il réalise des œuvres plus
+          originales portant un regard extrêmement critique sur la société
+          japonaise actuelle.
+          <br />
+          <br /> En 2012 dans The Land of Hope il aborde la question des
+          conséquences d'une catastrophe nucléaire. Naomi Kawase est distinguée
+          aussi bien pour ses fictions que pour ses documentaires
+          autobiographiques.
+          <br />
+          <br /> Elle est primée dans les festivals les plus prestigieux,
+          notamment le Grand prix au festival de Cannes 2007 pour le merveilleux
+          La Forêt de Mogari. Après 30 ans d'exil au Proche-Orient et quelques
+          années de prison, Masao Adachi revient à la réalisation en 2005 après
+          avoir été sollicité par des cinéphiles.
+          <br />
+          <br /> Au tournant des années 2010, c'est Kōji Wakamatsu qui revient à
+          la réalisation pour quatre films.
+          <br />
+          <br /> Il meurt en 2012, renversé par un taxi juste après avoir
+          annoncé sa volonté de réaliser un film sur l'entreprise Tepco et
+          l'accident nucléaire de Fukushima. Leurs films des années 1960 et 1970
+          sortent pour la première fois en Occident.
+        </p>
       </div>
       <div className='pictureRight'>
         <img src={VoyageTokyo} alt='voyageTokyo' />
@@ -182,6 +451,8 @@ export default function Histoire() {
         </p>
         <img className='godzilla' src={Godzilla} alt='godzilla' />
         <p className='description'>Affiche du film Godzilla (1954).</p>
+        <img className='kitano' src={Kitano} alt='kitano' />
+        <p className='description'>Takeshi Kitano</p>
       </div>
     </div>
   );
