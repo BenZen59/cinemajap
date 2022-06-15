d01c69f2064ca326bd9bb333c03235f3e51ef5a9
BenZen59 <benoit.zenone@gmail.com>
2022-06-15 18:18:45 +0200

    add genre

---
+++ src/components/Genre/Content.jsx
@@ -0,0 +1,28 @@
+const data = [
+  {
+    image:
+      'https://www.francetvinfo.fr/pictures/ORvfeYev6tADm7NDFE8qCRMfSQ0/1500x843/2016/08/23/astro_boy_1960_143_1280.jpg',
+    caption: 'Anime',
+    presentation: `Après quelques expérimentations au début du xxe siècle, le premier succès populaire du cinéma d'animation japonais (anime) 
+    est Astro, le petit robot, créé en 1963 par Osamu Tezuka et encouragé par le lobby nucléaire américain. Mais la reconnaissance internationale 
+    de l'anime ne vient que plus tard. "Akira" de Katsuhiro Ōtomo (1988) a un budget record pour l'animation japonaise et sort ensuite notamment 
+    aux États-Unis et en France. Les films du studio Ghibli et ses personnages font alors le tour du monde. "Le Voyage de Chihiro" d'Hayao Miyazaki 
+    reçoit le 1er prix du Festival du film de Berlin 2002 et remporte l'Oscar du meilleur film d'animation en 2003. Les films de Mamoru Oshii comme 
+    "Ghost in the Shell" sont aussi remarqués et le Festival de Cannes 2004 place "Ghost in the Shell 2: Innocence" en compétition officielle. 
+    Les autres réalisateurs d'anime les plus reconnus sont Isao Takahata, Osamu Dezaki, Yoshiaki Kawajiri, Satoshi Kon, Mamoru Hosoda, Makoto Shinkai, 
+    Hiroyuki Okiura.`,
+    movie: 'Princesse Mononoké, Ghost in the Sheel, Le voyage de Chihiro',
+  },
+  {
+    image:
+      'https://upload.wikimedia.org/wikipedia/commons/thumb/d/db/Kenjutsu_at_the_Japanese_Garden_03.jpg/220px-Kenjutsu_at_the_Japanese_Garden_03.jpg',
+    caption: 'Chanbara',
+    presentation: `Le chanbara (チャンバラ, ou chambara selon la méthode Hepburn traditionnelle) est un genre cinématographique et théâtral japonais 
+    de bataille de sabre souvent présenté comme un équivalent du film de cape et d'épée européen.
+    Le mot « chanbara » provient de la contraction des onomatopées « chan-chan bara-bara » qui désignent le bruit de la lame tranchant 
+    la chair. Le genre est également appelé ken geki (film de sabre) et est parfois assimilé à un sous-ensemble du « jidai-geki » (film historique).`,
+    movie: 'Hara-kiri, Les Sept Samouraïs, Kagemusha',
+  },
+];
+
+export default data;
--- src/components/Genre/Genre.jsx
+++ src/components/Genre/Genre.jsx
@@ -1,3 +1,52 @@
+import './Genre.css';
+import data from './Content';
+import { Button, Tooltip, Whisper } from 'rsuite';
+
 export default function Genre() {
-  return <div className='genre'>Genre</div>;
+  function speaker(caption, presentation, movie) {
+    const speaker = (
+      <Tooltip>
+        <div className='pTooltip'>
+          <p className='directorTooltip'>{caption}</p>
+          <p className='presentationTooltip'>{presentation}</p>
+          <p className='movieTooltip'>Films notables : {movie}</p>
+        </div>
+      </Tooltip>
+    );
+    return speaker;
+  }
+
+  return (
+    <>
+      {' '}
+      <h1 className='h1'>Liste des genres dans le cinéma japonais</h1>
+      <div className='realisateur'>
+        {data.map((event) => {
+          return (
+            <>
+              <div className='director'>
+                <img src={event.image} alt='director' />
+                <p className='nameDirector'>{event.caption}</p>
+                <Whisper
+                  controlId='control-id-container'
+                  preventOverflow
+                  trigger='click'
+                  speaker={speaker(
+                    event.caption,
+                    event.presentation,
+                    event.movie
+                  )}
+                  placement='auto'
+                >
+                  <Button className='button' appearance='primary'>
+                    En savoir plus
+                  </Button>
+                </Whisper>
+              </div>
+            </>
+          );
+        })}
+      </div>
+    </>
+  );
 }
