830a648241eb0ffdb737e1a35e043aaae663e950
BenZen59 <benoit.zenone@gmail.com>
2022-06-26 16:54:04 +0200

    add film

--- src/components/Film/Film.jsx
+++ src/components/Film/Film.jsx
@@ -1,3 +1,26 @@
+import './Film.css';
+import { Button } from 'rsuite';
+
 export default function Film() {
-  return <div className='film'>Films</div>;
+  return (
+    <div className='film'>
+      <div className='champs'>
+        <table className='texteChamps'>
+          <tr>
+            <th>Nom du film</th>
+            <th>Réalisateur</th>
+            <th>Année de sortie</th>
+          </tr>
+          <tr>
+            <td>Test 1</td>
+            <td>Test 2</td>
+            <td>Test 3</td>
+          </tr>
+        </table>
+      </div>
+      <Button className='button' appearance='primary'>
+        Ajouter un film
+      </Button>
+    </div>
+  );
 }
