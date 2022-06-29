6692e2f8b4875ae564a58016163f4fca76368c8a
BenZen59 <benoit.zenone@gmail.com>
2022-06-29 23:56:38 +0200

    add function addmovie update

--- src/components/Film/Add.jsx
+++ src/components/Film/Add.jsx
@@ -3,6 +3,7 @@ import './Add.css';
 export default function Add(props) {
   let data = [];
 
+  // eslint-disable-next-line no-unused-vars
   function addMovieFunction() {
     const nomFilm = document.getElementById('nomFilm').value;
     const realisateur = document.getElementById('realisateur').value;
